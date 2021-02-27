823cd2ff1d57c68c3c981639d3f6ab4de4dd5c15
KacperKotlewski <kotlewski.kacper@gmail.com>
2021-02-27 15:30:03 +0100

    fixed .md

--- bench.py
+++ bench.py
@@ -4,10 +4,10 @@ from core import rapidjson as rj
 from core import tests as tests
 
 def bench_print_result(elapsed, n_workers, n_cycles):
-    return ("\nNumber of workers: %s\t Elapsed: %.2f sec\t%.0d ns/op" % (n_workers, elapsed, (elapsed / n_cycles * 10 ** 9)))
+    return ("\n\nNumber of workers: %s\t Elapsed: %.2f sec\t%.0d ns/op" % (n_workers, elapsed, (elapsed / n_cycles * 10 ** 9)))
 def run_function_for_thread_test(function, name, n_cycles = 1000, n_workers_max=11 ):
-    text = (f"\n\n################ {name} ################")
-    text+=("\n################ Parsing ################")
+    text = (f"\n\n## {name}")
+    #text+=("\n################ Parsing ################")
     for n_workers in range(1, n_workers_max):
         elapsed = function(n_cycles, n_workers)
         text+=bench_print_result(elapsed, n_workers, n_cycles)
@@ -16,7 +16,7 @@ def run_function_for_thread_test(function, name, n_cycles = 1000, n_workers_max=
 def threads_test():
     n_cycles = 10000
     max_workers = 24
-    text =f"up to {max_workers} workers and {n_cycles} cycles each"
+    text =f"#bench test\nup to {max_workers} workers and {n_cycles} cycles each"
     text+=run_function_for_thread_test(function=tests.run_worker_test, name="parallel cython", n_cycles=n_cycles, n_workers_max=max_workers)
     text+=run_function_for_thread_test(function=tests.run_iteration_test, name="iterations range()", n_cycles=n_cycles, n_workers_max=max_workers)
     text+=run_function_for_thread_test(function=tests.run_threads_test, name="pythreads", n_cycles=n_cycles, n_workers_max=max_workers)
